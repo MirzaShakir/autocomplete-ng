e5393828fb0cb0f2de37f0d2bd432719810cbcd4
MirzaShakir <baigmirzashakir@gmail.com>
2019-10-30 00:07:03 +0530

    Added textbox on config reader component

--- src/app/configreader/configreader.component.ts
+++ src/app/configreader/configreader.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./configreader.component.css']
 })
 export class ConfigreaderComponent implements OnInit {
+  arrjsonWidgets = <any>[];
 
   constructor() { }
 
